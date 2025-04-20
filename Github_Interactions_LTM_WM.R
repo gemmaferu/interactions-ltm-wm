2b5fae516c44dd26d2bf787fb77a755d39699194
Gemma Fernández-Rubio <105786356+gemmaferu@users.noreply.github.com>
2025-04-20 16:43:18 +0200

    Github_Interactions_LTM_WM.R

--- Github_Interactions_LTM_WM.R
+++ Github_Interactions_LTM_WM.R
@@ -2,124 +2,67 @@
 
 #Gemma Fernández-Rubio
 #Center for Music in the Brain, Aarhus University, Aarhus (Denmark)
-#13-09-2024
+#01-04-2024
 
 #LIBRARIES, WORKING DIRECTORY AND DATA ====
 library(readxl)
-library(ggplot2)
-library(tidyverse)
-library(ggcorrplot)
-library(broom)
-library(ggfortify)
 library(dplyr)
-library(devtools)
-library(patchwork)
+library(psych)
+library(broom)
+library(effects)
+library(ggplot2)
+setwd('/YourPath')
+data <- read_excel('SupplementaryTable1.xlsx')
+data <- data %>% filter(dataset == 2021) #select 2021, 2022, or 2024
 
-setwd('/Your_Path')
+#DESCRIPTIVE STATISTICS ====
+describe(data[,c('age','wm','musictraining','mem_n','nov_n')]) #summarize main variables
 
-data_2021 <- read_excel('dataset_2021.xlsx')
-data_2022 <- read_excel('dataset_2022.xlsx')
-data_2024 <- read_excel('dataset_2024.xlsx')
-
-#2021 DATASET ====
-
-#GENERALIZED LINEAR MODEL (GLM)
+#GENERALIZED LINEAR MODELS (GLMs) ====
 #memorized sequences
-model_old_2021 <- glm(old ~ wm + age + musictraining, family = Gamma(link = 'log'), data = data_2021); summary(model_old_2021) #compute GLM
-AIC(model_old_2021) #check goodness-of-fit
-par(mfrow = c(2, 2)); plot(model_old_2021) #plot GLM
-sum_old_2021 <- tidy(model_old_2021); sum_old_2021[, 2:5] <- round(sum_old_2021[, 2:5], digits = 3) #summarize GLM
+model_mem <- glm(cbind(mem_n, mem_k - mem_n) ~ wm + age + musictraining, family = binomial(link='logit'), data = data); summary(model_mem) #compute GLM
+par(mfrow = c(2, 2)); plot(model_mem) #plot GLM
+sum_mem <- tidy(model_mem); sum_mem[, 2:5] <- round(sum_mem[, 2:5], digits = 3) #summarize GLM
+eff_mem <- data.frame(predictorEffects(model_mem, ~ wm)) #compute effects
+ggplot(eff_mem, aes(x = wm$wm, y = wm$fit)) + #plot effects
+  geom_line(color = 'dodgerblue', size = 1) +
+  geom_ribbon(aes(ymin = wm$lower, ymax = wm$upper), alpha = 0.2, fill = 'dodgerblue') +
+  scale_x_continuous(expand = c(0,0.5), breaks = seq(min(eff_mem$wm$wm), max(eff_mem$wm$wm), by = 2)) +
+  scale_y_continuous(breaks = seq(0, 1, by = 0.05)) +
+  labs(x = 'WM', y = 'Probability LTM-M') +
+  theme_bw() +
+  theme(text = element_text(size = 15, family = 'sans'))
 
 #novel sequences
-model_new_2021 <- glm(new ~ wm + age + musictraining, family = Gamma(link = 'log'), data = data_2021); summary(model_new_2021) #compute GLM
-AIC(model_new_2021) #check goodness-of-fit
-par(mfrow = c(2, 2)); plot(model_new_2021) #plot GLM
-sum_new_2021 <- tidy(model_new_2021); sum_new_2021[, 2:5] <- round(sum_new_2021[, 2:5], digits = 3) #summarize GLM
-
-#2022 DATASET ====
-
-#GENERALIZED LINEAR MODEL (GLM)
-#memorized sequences
-model_old_2022 <- glm(old ~ wm + age + musictraining, family = Gamma(link = 'log'), data = data_2022); summary(model_old_2022) #compute GLM
-AIC(model_old_2022) #check goodness-of-fit
-par(mfrow = c(2, 2)); plot(model_old_2022) #plot GLM
-sum_old_2022 <- tidy(model_old_2022); sum_old_2022[, 2:5] <- round(sum_old_2022[, 2:5], digits = 3) #summarize GLM
-
-#novel sequences
-model_new_2022 <- glm(new ~ wm + age + musictraining, family = Gamma(link = 'log'), data = data_2022); summary(model_new_2022) #compute GLM
-AIC(model_new_2022) #check goodness-of-fit
-par(mfrow = c(2, 2)); plot(model_new_2022) #plot GLM
-sum_new_2022 <- tidy(model_new_2022); sum_new_2022[, 2:5] <- round(sum_new_2022[, 2:5], digits = 3) #summarize GLM
-
-#2024 DATASET ====
-#RESHAPE DATA
-data_2024$old <- (data_2024$old_k1 + data_2024$old_k2)/2
-data_2024$new <- (data_2024$new_k1 + data_2024$new_k2)/2
-data_2024 <- data_2024 %>% filter(dem < 3)
-
-#GENERALIZED LINEAR MODEL (GLM)
-#memorized sequences
-model_old_2024 <- glm(old ~ wm + age + musictraining, family = Gamma(link = 'log'), data = data_2024); summary(model_old_2024) #compute GLM
-AIC(model_old_2024) #check goodness-of-fit
-par(mfrow = c(2, 2)); plot(model_old_2024) #plot GLM
-sum_old_2024 <- tidy(model_old_2024); sum_old_2024[, 2:5] <- round(sum_old_2024[, 2:5], digits = 3) #summarize GLM
-
-#novel sequences
-model_new_2024 <- glm(new ~ wm + age + musictraining, family = Gamma(link = 'log'), data = data_2024); summary(model_new_2024) #compute GLM
-AIC(model_new_2024) #check goodness-of-fit
-par(mfrow = c(2, 2)); plot(model_new_2024) #plot GLM
-sum_new_2024 <- tidy(model_new_2024); sum_new_2024[, 2:5] <- round(sum_new_2024[, 2:5], digits = 3) #summarize GLM
-
-#PLOTTING ====
-#2021 DATASET
-zscores_2021 <- data.frame(old = scale(data_2021$old), new = scale(data_2021$new), wm = scale(data_2021$wm))
-mem2021 <- ggplot(data = zscores_2021, mapping = aes(x = old, y = wm)) + 
-  geom_smooth(method = 'glm', color = 'black') + 
-  geom_point(alpha = 0.2) +
-  labs(x = 'LTM memorized (z-scores)', y = 'WM (z-scores)') +
+model_nov <- glm(cbind(nov_n, nov_k - nov_n) ~ wm + age + musictraining, family = binomial(link='logit'), data = data); summary(model_nov) #compute GLM
+par(mfrow = c(2, 2)); plot(model_nov) #plot GLM
+sum_nov <- tidy(model_nov); sum_nov[, 2:5] <- round(sum_nov[, 2:5], digits = 3) #summarize GLM
+eff_nov <- data.frame(predictorEffects(model_nov, ~ wm)) #compute effects
+ggplot(eff_nov, aes(x = wm$wm, y = wm$fit)) + #plot effects
+  geom_line(color = 'dodgerblue', size = 1) +
+  geom_ribbon(aes(ymin = wm$lower, ymax = wm$upper), alpha = 0.2, fill = 'dodgerblue') +
+  scale_x_continuous(expand = c(0,0.5), breaks = seq(min(eff_nov$wm$wm), max(eff_nov$wm$wm), by = 2)) +
+  scale_y_continuous(breaks = seq(.1, 1, by = 0.05)) +
+  labs(x = 'WM', y = 'Probability LTM-N') +
   theme_bw() +
-  theme(text = element_text(size = 12, family = 'sans'))
+  theme(text = element_text(size = 15, family = 'sans'))
 
-nov2021 <- ggplot(data = zscores_2021, mapping = aes(x = new, y = wm)) + 
-  geom_smooth(method = 'glm', color = 'black') + 
-  geom_point(alpha = 0.2) +
-  labs(x = 'LTM novel (z-scores)', y = 'WM (z-scores)') +
-  theme_bw() +
-  theme(text = element_text(size = 12, family = 'sans'))
+#nt1 sequences
+model_novt1 <- glm(cbind(novt1_n, novt1_k - novt1_n) ~ wm + age + musictraining, family = binomial(link = 'logit'), data = data); summary(model_novt1) #compute GLM
+par(mfrow = c(2, 2)); plot(model_novt1) #plot GLM
+sum_novt1 <- tidy(model_novt1); sum_novt1[, 2:5] <- round(sum_novt1[, 2:5], digits = 3) #summarize GLM
 
-#2022 DATASET
-zscores_2022 <- data.frame(old = scale(data_2022$old), new = scale(data_2022$new), wm = scale(data_2022$wm))
-mem2022 <- ggplot(data = zscores_2022, mapping = aes(x = old, y = wm)) + 
-  geom_smooth(method = 'glm', color = 'black') + 
-  geom_point(alpha = 0.2) +
-  labs(x = 'LTM memorized (z-scores)', y = 'WM (z-scores)') +
-  theme_bw() +
-  theme(text = element_text(size = 12, family = 'sans'))
+#nt2 sequences
+model_novt2 <- glm(cbind(novt2_n, novt2_k - novt2_n) ~ wm + age + musictraining, family = binomial(link = 'logit'), data = data); summary(model_novt2) #compute GLM
+par(mfrow = c(2, 2)); plot(model_novt2) #plot GLM
+sum_novt2 <- tidy(model_novt2); sum_novt2[, 2:5] <- round(sum_novt2[, 2:5], digits = 3) #summarize GLM
 
-nov2022 <- ggplot(data = zscores_2022, mapping = aes(x = new, y = wm)) + 
-  geom_smooth(method = 'glm', color = 'black') + 
-  geom_point(alpha = 0.2) +
-  labs(x = 'LTM novel (z-scores)', y = 'WM (z-scores)') +
-  theme_bw() +
-  theme(text = element_text(size = 12, family = 'sans'))
+#nt3 sequences
+model_novt3 <- glm(cbind(novt3_n, novt3_k - novt3_n) ~ wm + age + musictraining, family = binomial(link = 'logit'), data = data); summary(model_novt3) #compute GLM
+par(mfrow = c(2, 2)); plot(model_novt3) #plot GLM
+sum_novt3 <- tidy(model_novt3); sum_novt3[, 2:5] <- round(sum_novt3[, 2:5], digits = 3) #summarize GLM
 
-#2024 DATASET
-zscores_2024 <- data.frame(old = scale(data_2024$old), new = scale(data_2024$new), wm = scale(data_2024$wm))
-mem2024 <- ggplot(data = zscores_2024, mapping = aes(x = old, y = wm)) + 
-  geom_smooth(method = 'glm', color = 'black') + 
-  geom_point(alpha = 0.2) +
-  labs(x = 'LTM memorized (z-scores)', y = 'WM (z-scores)') +
-  theme_bw() +
-  theme(text = element_text(size = 12, family = 'sans'))
-
-nov2024 <- ggplot(data = zscores_2024, mapping = aes(x = new, y = wm)) + 
-  geom_smooth(method = 'glm', color = 'black') + 
-  geom_point(alpha = 0.2) +
-  labs(x = 'LTM novel (z-scores)', y = 'WM (z-scores)') +
-  theme_bw() +
-  theme(text = element_text(size = 12, family = 'sans'))
-
-(mem2021 | nov2021) / (mem2022 | nov2022) / (mem2024 | nov2024)
-ggsave('Figure_1.pdf')
-
-
+#nt4 sequences
+model_novt4 <- glm(cbind(novt4_n, novt4_k - novt4_n) ~ wm + age + musictraining, family = binomial(link = 'logit'), data = data); summary(model_novt4) #compute GLM
+par(mfrow = c(2, 2)); plot(model_novt4) #plot GLM
+sum_novt4 <- tidy(model_novt4); sum_novt4[, 2:5] <- round(sum_novt4[, 2:5], digits = 3) #summarize GLM
